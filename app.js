8369140f2132b8847ff82f60d1e98b197e5ff20b
snow <mlafuente98@gmail.com>
2024-01-03 17:15:44 +0100

    ajout express et remodelage du server pour les erreurs

--- app.js
+++ app.js
@@ -8,11 +8,17 @@ app.use((req, res, next) =>{
 });
 
 app.use((req, res, next) => {
-    
+    res.status(201);
+    next();
 });
 
 app.use((req, res, next) => {
     res.json({message:'la requete est la i guess 2.0'})
+    next();
+});
+
+app.use ((req, res, next) => {
+    console.log('La requete a été envoyée avec succès !');
 });
 
 module.exports = app;
