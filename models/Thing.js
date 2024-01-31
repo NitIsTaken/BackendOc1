3311b1a705bf5dd6795afdabee03d177ec9ba11b
NitIsTaken <mlafuente98@gmail.com>
2024-01-31 01:10:48 +0100

    version pc

--- models/Thing.js
+++ models/Thing.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const thingSchema = mongoose.Schema({
     title: {type: String, required: true},
-    description: {type: String, require: true},
-    imageUrl: {type: String, require: true},
-    userId: {type: String, require: true},
-    price: {type: Number, require: true},
+    description: {type: String, required: true},
+    imageUrl: {type: String, required: true},
+    userId: {type: String, required: true},
+    price: {type: Number, required: true},
 });
 
 module.exports = mongoose.model('Thing', thingSchema);
