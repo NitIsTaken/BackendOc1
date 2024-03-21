15223b0d734ad515eb5543d860b8f5bf4246f2af
NitIsTaken <mlafuente98@gmail.com>
2024-03-21 14:53:43 +0100

    rajout des dernières parties

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require('express');
-const bpdyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Thing = require('./models/Thing');
-
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://LafuenteM:MotDePasseMongoDB@ocbackend.heqqnoe.mongodb.net/?retryWrites=true&w=majority',
 { useNewUrlParser: true,
     useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
-    
 
 const app = express();
 
@@ -23,38 +22,6 @@ app.use((req, res, next) => {
     next();
   });
 
-app.post('/api/stuff', (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body
-  });
-  thing.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.put('/api/stuff/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id}, {...req.body, _id: req.params.id})
-      .then(() => res.status(200).json({message: 'Objet modifié !'}))
-      .catch(error => res.status(400).json({error}));
-});
-
-app.delete('/api/stuff/:id', (req, res, next)=> {
-    Thing.deleteOne({ _id: req.params.id})
-      .then(() => res.status(200).json({message: 'Objet supprimé !'}))
-      .catch(error => res.status(400).json({error}));
-});
-
-app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-      .then(things => res.status(200).json(things))
-      .catch(error => res.status(404).json({error}));
-});
-
-app.get('/api/stuff',(req, res, next) => {
-    Thing.find()
-      .then(things => res.status(200).json(things))
-      .catch(error => res.status(400).json({error}));
-});
-
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 module.exports = app;
